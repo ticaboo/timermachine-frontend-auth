739ba79f3502df0ad7801290425fe86dafd3fc89
Henry Kemp <henry@ticaboo.co.uk>
2022-11-24 12:24:12 +0000

    emailpwd

--- src/config.tsx
+++ src/config.tsx
@@ -3,6 +3,7 @@ import ThirdPartyEmailPassword, {
   Github,
   Apple
 } from 'supertokens-auth-react/recipe/thirdpartyemailpassword';
+import EmailPassword from 'supertokens-auth-react/recipe/emailpassword';
 import Session from 'supertokens-auth-react/recipe/session';
 
 export const SuperTokensConfig = {
@@ -10,17 +11,18 @@ export const SuperTokensConfig = {
     appName: 'From SuperTokens Demo App',
     apiDomain: 'https://app.timermachine.com',
     apiBasePath: '/.netlify/functions/auth',
-    websiteDomain: 'https://front.timermachine.com'
-    //websiteBasePath: '/auth'
+    websiteDomain: 'https://front.timermachine.com',
+    websiteBasePath: '/member'
   },
   // recipeList contains all the modules that you want to
   // use from SuperTokens. See the full list here: https://supertokens.com/docs/guides
   recipeList: [
-    ThirdPartyEmailPassword.init({
-      signInAndUpFeature: {
-        providers: [Github.init(), Google.init(), Apple.init()]
-      }
-    }),
+    // ThirdPartyEmailPassword.init({
+    //   signInAndUpFeature: {
+    //     providers: [Github.init(), Google.init(), Apple.init(), ]
+    //   }
+    // }),
+    EmailPassword.init(),
     Session.init()
   ]
 };
